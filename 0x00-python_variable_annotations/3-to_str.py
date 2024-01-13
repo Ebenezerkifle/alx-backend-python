d9a10e21ef19564448c8257ee6abd256eb695726
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-13 12:02:07 +0300

    float to string convertion

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic type-annotated method to_str"""
+
+def to_str(n: float) -> str:
+    """type-annotated method to_str that takes n which is float
+    and returns a string representation of a float value"""
+    return str(n)
